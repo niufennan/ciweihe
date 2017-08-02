cb751fc3c25b6d1d71f0f935efd0f876a0ff5e2d
niufennan <626211114@qq.com>
2017-08-02 16:47:37 +0800

    起步

---
+++ ciweihe.wxchat/src/main/java/cn/ciweihe/wxchat/WxchatApplication.java
@@ -0,0 +1,12 @@
+package cn.ciweihe.wxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxchatApplication.class, args);
+	}
+}
