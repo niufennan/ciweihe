a1a2fbb1b27983849b20192d28f7dcc3b30525fb
niufennan <jiangchao226@126.com>
2017-08-15 23:42:51 +0800

    安全框架

---
+++ ciweihe.manage/src/main/java/cn/ciweihe/manage/security/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package cn.ciweihe.manage.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2017/8/15.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+    //@Override
+   // public configure
+}
