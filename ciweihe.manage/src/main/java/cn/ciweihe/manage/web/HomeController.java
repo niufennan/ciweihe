7cfc82a683c67c7746a31b9957de03c0ceff791c
niufennan <jiangchao226@126.com>
2017-08-20 23:37:51 +0800

    继续首页

--- ciweihe.manage/src/main/java/cn/ciweihe/manage/web/HomeController.java
+++ ciweihe.manage/src/main/java/cn/ciweihe/manage/web/HomeController.java
@@ -1,6 +1,7 @@
 package cn.ciweihe.manage.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping(value = {"/","index"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name", "Dear");
         return "home/index";
     }
 }
